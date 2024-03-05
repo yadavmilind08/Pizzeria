9336ba68e35a57ce6a77606b6f4a60c3b981d946
Milind Yadav <yadavmilind08@gmail.com>
2024-03-06 00:06:07 +0530

    Order list

--- client/src/app/orders/order-list/order-list.component.ts
+++ client/src/app/orders/order-list/order-list.component.ts
@@ -3,8 +3,58 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-order-list',
   templateUrl: './order-list.component.html',
-  styleUrls: ['./order-list.component.scss']
+  styleUrls: ['./order-list.component.scss'],
 })
 export class OrderListComponent {
-
+  orderList = [
+    {
+      id: 1,
+      orderDate: new Date().toISOString(),
+      totalAmount: 50,
+      pizzas: [
+        {
+          id: 1,
+          name: 'pizza1',
+          ingredients: [
+            { name: 'Medium', price: 7.5, type: 'size' },
+            { name: 'Marinara', price: 0.5, type: 'sauce' },
+            { name: 'Extra Mozzarella', price: 1.5, type: 'cheese' },
+            { name: 'Pepperoni', price: 0.75, type: 'topping' },
+            { name: 'Mushrooms', price: 0.5, type: 'topping' },
+          ],
+        },
+        {
+          id: 2,
+          name: 'pizza2',
+          ingredients: [
+            { name: 'Medium', price: 7.5, type: 'size' },
+            { name: 'Ranch', price: 1.5, type: 'sauce' },
+            { name: 'Mozzarella', price: 0.5, type: 'cheese' },
+            { name: 'Pepperoni', price: 0.75, type: 'topping' },
+            { name: 'Italian Sausage', price: 0.5, type: 'topping' },
+            { name: 'Pineapple', price: 1.0, type: 'topping' },
+            { name: 'Mushrooms', price: 0.5, type: 'topping' },
+          ],
+        },
+      ],
+    },
+    {
+      id: 1,
+      orderDate: new Date().toISOString(),
+      totalAmount: 80,
+      pizzas: [
+        {
+          id: 1,
+          name: 'pizza1',
+          ingredients: [
+            { name: 'Medium', price: 7.5, type: 'size' },
+            { name: 'Marinara', price: 0.5, type: 'sauce' },
+            { name: 'Extra Mozzarella', price: 1.5, type: 'cheese' },
+            { name: 'Pepperoni', price: 0.75, type: 'topping' },
+            { name: 'Mushrooms', price: 0.5, type: 'topping' },
+          ],
+        },
+      ],
+    },
+  ];
 }
--- client/src/app/pizza/menu/menu.component.ts
+++ client/src/app/pizza/menu/menu.component.ts
@@ -5,4 +5,60 @@ import { Component } from '@angular/core';
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {}
+export class MenuComponent {
+  pizzaList = [
+    {
+      id: 1,
+      name: 'pizza1',
+      ingredients: [
+        { name: 'Medium', price: 7.5, type: 'size' },
+        { name: 'Marinara', price: 0.5, type: 'sauce' },
+        { name: 'Extra Mozzarella', price: 1.5, type: 'cheese' },
+        { name: 'Pepperoni', price: 0.75, type: 'topping' },
+        { name: 'Mushrooms', price: 0.5, type: 'topping' },
+      ],
+    },
+    {
+      id: 2,
+      name: 'pizza2',
+      ingredients: [
+        { name: 'Medium', price: 7.5, type: 'size' },
+        { name: 'Ranch', price: 1.5, type: 'sauce' },
+        { name: 'Mozzarella', price: 0.5, type: 'cheese' },
+        { name: 'Pepperoni', price: 0.75, type: 'topping' },
+        { name: 'Italian Sausage', price: 0.5, type: 'topping' },
+        { name: 'Pineapple', price: 1.0, type: 'topping' },
+        { name: 'Mushrooms', price: 0.5, type: 'topping' },
+      ],
+    },
+  ];
+
+  selectedPizzas: any[] = [];
+
+  onAdd(pizza: any) {
+    this.selectedPizzas.push(pizza);
+  }
+
+  onRemove(pizza: any) {
+    const index = this.selectedPizzas.findIndex((x) => x.name === pizza.name);
+    if (index > -1) {
+      this.selectedPizzas.splice(index, 1);
+    }
+  }
+
+  calculateQuantity(name: string) {
+    return this.selectedPizzas.filter((x) => x.name === name).length;
+  }
+
+  calculateTotalPrice(): number {
+    return this.selectedPizzas.reduce(
+      (total, pizza) =>
+        total +
+        pizza.ingredients.reduce(
+          (acc: any, ingredient: { price: any }) => acc + ingredient.price,
+          0
+        ),
+      0
+    );
+  }
+}
